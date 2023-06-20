7c789997263ee2c58157b25f9d1bceccb9b52e2d
Enmanuel Domínguez <manuelx_05@hotmail.com>
2023-06-20 19:05:11 -0400

    menu toggle

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# js-simple-game-menu
+# Dynamic JavaScript Menu
+
+This project is a simple and reusable JavaScript module for creating dynamic menus on a web page. It's highly configurable and easy to integrate into your existing web projects.
+
+## Features
+
+- Dynamically creates a menu based on passed configuration object.
+- Supports nested submenus.
+- Provides click handlers for each menu item.
+- Allows you to set custom names for the 'Close' and 'Show Menu' buttons.
+- Menu's title and subtitle can be customized.
+
+## Usage
+
+To use this menu in your project, follow these steps:
+
+1. Include the `menu.js` and `menu.css` files in your project.
+
+2. Create a configuration object for your menu. The configuration object should have the following properties:
+
+   - `menuData`: an array of objects, each representing a menu item. Each menu item should have a `label` property and an `onClick` function. If a menu item has a submenu, it should also have a `submenu` property that is an array of submenu items.
+
+   - `title`: a string representing the title of the menu. This is optional.
+
+   - `subtitle`: a string representing the subtitle of the menu. This is optional.
+
+   - `buttonNames`: an object with properties `close` and `show` specifying the text for the close button in the menu and the show menu button, respectively. If you don't specify `buttonNames`, the default values 'Close' for the close button and 'Show Menu' for the show menu button will be used. This is optional.
+
+3. Instantiate a new `Menu` object using your configuration object:
+
+```javascript
+const menuConfig = {
+  menuData: [
+    // your menu items here
+  ],
+  title: 'Your Menu Title',
+  subtitle: 'Your Menu Subtitle',
+  buttonNames: { close: 'Your Close Text', show: 'Your Show Text' }
+};
+
+const menu = new Menu(menuConfig);
+```
+
+4. Call the `createMenu()` method on your `Menu` object to create the menu.
+
+5. Add a button to your page that calls the `toggle()` method on your `Menu` object to show or hide the menu.
+
+That's it! You now have a dynamic JavaScript menu in your project.
--- menu.js
+++ menu.js
@@ -1,66 +1,104 @@
 class Menu {
-    constructor(menuData) {
-      this.menuData = menuData;
-      this.menuElement = null;
-    }
-  
-    createMenuItems(menuData) {
-      const menuList = document.createElement('ul');
-  
-      for (let i = 0; i < menuData.length; i++) {
-        const menuItem = menuData[i];
-        const menuItemElement = document.createElement('li');
-  
-        menuItemElement.textContent = menuItem.label;
-  
-        if (menuItem.submenu && menuItem.submenu.length > 0) {
-          const submenu = this.createMenuItems(menuItem.submenu);
-          submenu.classList.add('submenu');
-          menuItemElement.appendChild(submenu);
-  
-          menuItemElement.addEventListener('click', function () {
-            this.childNodes[1].style.display = this.childNodes[1].style.display === 'block' ? 'none' : 'block';
-          });
-        } else {
-          menuItemElement.addEventListener('click', () => {
-            menuItem.onClick();
-            this.hide();
-          });
-        }
-  
-        menuList.appendChild(menuItemElement);
-      }
-  
-      return menuList;
-    }
-  
-    createMenu() {
-      const menuList = this.createMenuItems(this.menuData);
-  
-      const closeButton = document.createElement('button');
-      closeButton.textContent = 'Close';
-      closeButton.addEventListener('click', () => {
-        this.hide();
-      });
-  
-      this.menuElement = document.createElement('div');
-      this.menuElement.classList.add('menu', 'menu-container');
-      this.menuElement.appendChild(menuList);
-      this.menuElement.appendChild(closeButton);
-      document.body.appendChild(menu.menuElement);
-
-    }
-  
-    toggle() {
-      this.menuElement.classList.toggle('visible');
-    }
-  
-    show() {
-      this.menuElement.classList.add('visible');
-    }
-  
-    hide() {
-      this.menuElement.classList.remove('visible');
-    }
+  // Constructor initializes the menu properties
+  constructor({ menuData, title, subtitle, buttonNames }) {
+    this.menuData = menuData;      // Array of menu items
+    this.menuElement = null;      
+    this.title = title;           // Optional menu title
+    this.subtitle = subtitle;     // Optional menu subtitle
+    this.buttonNames = buttonNames || { close: 'Close', show: 'Show Menu' };  
+                                 // Default button names
   }
-  
+
+  // Creates menu items recursively
+  createMenuItems(menuData) {
+    const menuList = document.createElement('ul');  // Create the <ul> element
+
+    for (let i = 0; i < menuData.length; i++) {
+      const menuItem = menuData[i];             // Get the menu item object
+      const menuItemElement = document.createElement('li'); // Create <li> element
+
+      menuItemElement.textContent = menuItem.label;   // Set menu item label
+
+      // If menu item has a submenu, create it recursively
+      if (menuItem.submenu && menuItem.submenu.length > 0) {  
+        const submenu = this.createMenuItems(menuItem.submenu);
+        submenu.classList.add('submenu');         
+        submenu.style.display = 'none';          
+        // Make submenu hidden initially  
+
+        menuItemElement.appendChild(submenu);      
+        menuItemElement.addEventListener('click', (e) => {
+          e.stopPropagation();  
+          // Stop event bubbling to prevent hiding all submenus
+          if (submenu.style.display === 'none') {  
+            this.hideSubmenu(menuList);  
+          }
+          this.toggleSubmenu(submenu);     // Toggle submenu visibility
+        });
+      } else {
+        menuItemElement.addEventListener('click', menuItem.onClick);  
+                                 // Add click handler for menu item
+      }
+
+      menuList.appendChild(menuItemElement); // Add menu item to list
+    }
+
+    return menuList;  // Return the menu list
+  }
+
+  // Hides all submenus in a menu list
+  hideSubmenu(list) {
+    const submenus = list.querySelectorAll('.submenu');
+    submenus.forEach(submenu => {
+      submenu.style.display = 'none';  
+      this.hideSubmenu(submenu);      // Hide submenus recursively
+    });
+  }
+
+  // Creates the entire menu
+  createMenu() {
+    const menuList = this.createMenuItems(this.menuData);  // Create menu items
+    
+    const closeButton = document.createElement('button');
+    closeButton.textContent = this.buttonNames.close;
+    closeButton.addEventListener('click', () => {
+      this.hide();                  // Hide menu when close button is clicked
+    });
+
+    this.menuElement = document.createElement('div');
+    this.menuElement.classList.add('menu', 'menu-container');
+
+    if (this.title) {               // Add title if provided
+      const titleElement = document.createElement('div');
+      titleElement.classList.add('menu-title');
+      titleElement.textContent = this.title;
+      this.menuElement.appendChild(titleElement);
+    }
+
+    if (this.subtitle) {           // Add subtitle if provided  
+      const subtitleElement = document.createElement('div');
+      subtitleElement.classList.add('menu-subtitle');
+      subtitleElement.textContent = this.subtitle;
+      this.menuElement.appendChild(subtitleElement);
+    }
+
+    this.menuElement.appendChild(menuList);  
+    this.menuElement.appendChild(closeButton);
+    document.body.appendChild(this.menuElement);  
+  }
+
+  // Toggles menu visibility
+  toggle() { 
+    this.menuElement.classList.toggle('visible');
+  }
+
+  // Hides the menu
+  hide() { 
+    this.menuElement.classList.remove('visible');
+  }
+
+  // Toggles submenu
+  toggleSubmenu(submenu) {
+    submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
+  }
+}
